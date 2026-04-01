9d418c571f2407a33256e96228b0063eff97ab10
Kapten boneng <iwak01bogo@gmail.com>
2026-04-02 01:22:46 +0700

    Create piHealth.js

---
+++ services/piHealth.js
@@ -0,0 +1,19 @@
+// services/piHealth.js
+import fetch from "node-fetch";
+
+export async function getPiHealth() {
+  const res = await fetch("https://rpc.testnet.minepi.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: 1,
+      method: "getHealth"
+    })
+  });
+
+  const data = await res.json();
+  return data.result || data;
+}
